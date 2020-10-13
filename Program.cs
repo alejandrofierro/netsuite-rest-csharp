c86d155fb0240295255221bb2351a77c6be276fb
Alejandro Fierro <alejandro.fierro@gmail.com>
2020-10-13 02:01:37 -0500

    Initial Release
    Signed-off-by: Alejandro Fierro <alejandro.fierro@gmail.com>

---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿// ===============================
+// AUTHOR           : Alejandro Fierro
+// DATE             : 01 Sep 2020
+// PURPOSE          : Show how to consume Netsuite REST Services (RESTlet or REST API)
+// ===============================
+// Change History:
+//    - 01 Sep 2020 
+//          Creation
+//==================================
+
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using OAuth;
+using Newtonsoft.Json;
+
+namespace netsuiteToken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Fill These vars with your netsuite information **********************************************************************
+                String urlString = "";              //RESTlet uri
+                String ckey = "";                   //Consumer Key
+                String csecret = "";                //Consumer Secret
+                String tkey = "";                   //Token ID
+                String tsecret = "";                //Token Secret
+                String netsuiteAccount = "";        //Netsuite Account to connect to (i.e 5624525_SB1)
+                //**********************************************************************************************************************
+
+                Uri url = new Uri(urlString);
+                OAuthBase req = new OAuthBase();
+                String timestamp = req.GenerateTimeStamp();
+                String nonce = req.GenerateNonce();                
+                String norm = "";
+                String norm1 = "";
+                String signature = req.GenerateSignature(url, ckey, csecret, tkey, tsecret, "GET", timestamp, nonce, out norm, out norm1);
+                
+                //Percent Encode (Hex Escape) plus character
+                if (signature.Contains("+"))
+                {
+                    signature = signature.Replace("+", "%2B");
+                }
+
+                String header = "Authorization: OAuth ";
+                header += "oauth_signature=\"" + signature + "\",";
+                header += "oauth_version=\"1.0\",";
+                header += "oauth_nonce=\"" + nonce + "\",";
+                header += "oauth_signature_method=\"HMAC-SHA1\",";
+                header += "oauth_consumer_key=\"" + ckey + "\",";
+                header += "oauth_token=\"" + tkey + "\",";
+                header += "oauth_timestamp=\"" + timestamp + "\",";
+                header += "realm=\""+ netsuiteAccount +"\"";
+                HttpWebRequest request =
+               (HttpWebRequest)WebRequest.Create(urlString);
+
+                request.ContentType = "application/json";
+                request.Method = "GET";
+                request.Headers.Add(header);
+                WebResponse response = request.GetResponse();
+                HttpWebResponse httpResponse = (HttpWebResponse)response;
+
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                    Console.WriteLine(JsonConvert.DeserializeObject(reader.ReadToEnd()));
+                }
+
+            }
+            catch (System.Net.WebException e)
+            {
+                HttpWebResponse response = e.Response as HttpWebResponse;
+                switch ((int)response.StatusCode)
+                {
+                    case 401:
+                        Console.WriteLine("Unauthorized, please check your credentials");
+                        break;
+                    case 403:
+                        Console.WriteLine("Forbidden, please check your credentials");
+                        break;
+                    case 404:
+                        Console.WriteLine("Invalid Url");
+                        break;
+                }
+                Console.WriteLine("Code: " + (int)response.StatusCode);
+
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                    Console.WriteLine("Response: " + JsonConvert.DeserializeObject(reader.ReadToEnd()));
+
+                }
+
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# netsuite-rest-csharp
+# Netsuite REST C#
+
 This ia an example of how to call Netsuite's RESTlet or REST API Using OAuth1.0 with C#
+
+## Prerequisites
+> This has been tested using dotnet for linux version 3.1.402 on ubuntu 20.04, but it should work on Mac or Windows
+
+Install `dotnet` using the official documentation from microsoft [Install on ubuntu guide](https://docs.microsoft.com/es-mx/dotnet/core/install/linux-ubuntu)
+
+## How to test
+>This example uses a GET Method
+1. Clone Repository
+2. cd into **netsuite-rest-chsarp** folder
+3. Edit **Program.cs** file
+4. Fill **Consumer Token, Consumer Secret, Token Key, Token Secret** and **Account** with the netsuite tokens from your netsuite account.
+5. Fill the **Url** you want to call 
+6. Execute command `dotnet run`
